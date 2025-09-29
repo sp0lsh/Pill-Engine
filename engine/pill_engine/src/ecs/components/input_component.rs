f1363b8536bab15489d445c16cec51d5274db745
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2025-09-29 16:11:19 +0200

    Add Basic gamepad implementation.
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@gmail.com>

--- engine/pill_engine/src/ecs/components/input_component.rs
+++ engine/pill_engine/src/ecs/components/input_component.rs
@@ -4,8 +4,9 @@ use crate::{
 };
 
 use pill_core::{ PillTypeMapKey, Vector2f };
+use bitvec::prelude::*;
 
-use std::{ 
+use std::{
     any::Any,
     cell::RefCell,
     collections::HashMap,
@@ -14,24 +15,56 @@ use winit::dpi::PhysicalPosition;
 use winit::event::{ ElementState, MouseScrollDelta };
 use anyhow::{ Result, Context, Error };
 
+pub const GAMEPAD_DEADZONE: f32 = 0.05; // Deadzone for gamepad axes
+
+pub const KEYBOARD_KEY_COUNT: usize = KeyboardKey::F35 as usize + 1; // Total number of keys in KeyboardKey enum
+pub const MOUSE_BUTTON_COUNT: usize = 3; // Left, Middle, Right
+
+/// Gamepad enums with more descriptive names
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+pub enum GamepadButton {
+    A = 0, B, X, Y,
+    LeftBumper, RightBumper,
+    LeftTrigger, RightTrigger, // listed twice for convenience
+    Back, Start, Mode,
+    LeftStick, RightStick,
+    DPadUp, DPadDown, DPadLeft, DPadRight,
+}
+pub const GAMEPAD_BUTTON_COUNT: usize = GamepadButton::DPadRight as usize + 1;
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+pub enum GamepadAxis {
+    LeftStickX = 0, LeftStickY,
+    RightStickX, RightStickY,
+    LeftTrigger, RightTrigger,
+}
+pub const GAMEPAD_AXIS_COUNT: usize = GamepadAxis::RightTrigger as usize + 1;
+
 pub enum InputEvent {
     KeyboardKey { key: KeyboardKey, state: ElementState },
     MouseButton { key: MouseButton, state: ElementState },
     MouseWheel { delta: MouseScrollDelta },
     MouseDelta { delta: Vector2f },
-    MousePosition { position: Vector2f }
+    MousePosition { position: Vector2f },
+    GamepadButton { button: GamepadButton, state: ElementState },
+    GamepadAxis { axis: GamepadAxis, value: f32 },
 }
+
+pub type KeyState = BitArray<[u64; 4]>; // actually we have less but this is fine
+pub type GamepadButtonState = BitArray<[u32; 1]>; // we have 17 buttons
 
 pub struct InputComponent {
     // Keyboard arrays
-    pub(crate) pressed_keyboard_keys: [bool; 163],
-    pub(crate) released_keyboard_keys: [bool; 163],
-    pub(crate) keyboard_keys: [bool; 163],
+    pub(crate) pressed_keyboard_keys: KeyState,
+    pub(crate) released_keyboard_keys: KeyState,
+    pub(crate) keyboard_keys: KeyState,
 
     // Mouse buttons arrays
-    pub(crate) pressed_mouse_buttons: [bool; 3],
-    pub(crate) released_mouse_buttons: [bool; 3],
-    pub(crate) mouse_buttons: [bool; 3],
+    pub(crate) pressed_mouse_buttons: [bool; MOUSE_BUTTON_COUNT],
+    pub(crate) released_mouse_buttons: [bool; MOUSE_BUTTON_COUNT],
+    pub(crate) mouse_buttons: [bool; MOUSE_BUTTON_COUNT],
 
     // Mouse motion
     pub(crate) current_mouse_delta: Vector2f,
@@ -40,70 +73,83 @@ pub struct InputComponent {
     // Mouse scroll wheels delta
     pub(crate) current_mouse_scroll_delta: Vector2f,
     pub(crate) current_mouse_scroll_pixel_delta: Vector2f,
+
+    // Gamepad buttons and axes
+    pub(crate) pressed_gamepad_buttons: GamepadButtonState,
+    pub(crate) released_gamepad_buttons: GamepadButtonState,
+    pub(crate) gamepad_buttons: GamepadButtonState,
+    pub(crate) gamepad_axes: [f32; GAMEPAD_AXIS_COUNT],
 }
 
 impl InputComponent {
-    pub(crate) fn new() -> Self {
-        Self { 
-            pressed_keyboard_keys: [false; 163],
-            released_keyboard_keys: [false; 163],
-            keyboard_keys: [false; 163],
+    pub fn new() -> Self {
+        Self {
+            pressed_keyboard_keys: KeyState::ZERO,
+            released_keyboard_keys: KeyState::ZERO,
+            keyboard_keys: KeyState::ZERO,
 
-            pressed_mouse_buttons: [false; 3],
-            released_mouse_buttons: [false; 3],
-            mouse_buttons: [false; 3],
-    
+            pressed_mouse_buttons: [false; MOUSE_BUTTON_COUNT],
+            released_mouse_buttons: [false; MOUSE_BUTTON_COUNT],
+            mouse_buttons: [false; MOUSE_BUTTON_COUNT],
+
             current_mouse_delta: Vector2f::new(0.0, 0.0),
             current_mouse_position: Vector2f::new(0.0, 0.0),
 
             current_mouse_scroll_delta: Vector2f::new(0.0, 0.0),
             current_mouse_scroll_pixel_delta: Vector2f::new(0.0, 0.0),
+
+            pressed_gamepad_buttons: GamepadButtonState::ZERO,
+            released_gamepad_buttons: GamepadButtonState::ZERO,
+            gamepad_buttons: GamepadButtonState::ZERO,
+            gamepad_axes: [0.0; GAMEPAD_AXIS_COUNT],
         }
     }
 
-    pub(crate) fn set_keys(&mut self) {
-        for i in 0..163 {
-            if self.keyboard_keys[i] && self.pressed_keyboard_keys[i] {
-                self.pressed_keyboard_keys[i] = false;
-            }
-            if !self.keyboard_keys[i] && self.released_keyboard_keys[i] {
-                self.released_keyboard_keys[i] = false;
-            }
-        }
+    // frame-reset
+    pub fn clear_transient_states(&mut self) {
+        self.reset_keyboard();
+        self.reset_mouse_buttons();
+        self.reset_gamepad_buttons();
+        self.reset_mouse_motion();
     }
 
-    pub(crate) fn set_mouse_buttons(&mut self) {
-        for i in 0..3 {
-            if self.mouse_buttons[i] && self.pressed_mouse_buttons[i] {
-                self.pressed_mouse_buttons[i] = false;
-            }
-            if !self.mouse_buttons[i] && self.released_mouse_buttons[i] {
-                self.released_mouse_buttons[i] = false;
-            }
-        }
+    fn reset_keyboard(&mut self) {
+        self.pressed_keyboard_keys = KeyState::ZERO;
+        self.released_keyboard_keys = KeyState::ZERO;
     }
 
-    pub(crate) fn set_mouse_motion(&mut self) {
-        self.current_mouse_delta = Vector2f::new(0.0,0.0);
+    fn reset_mouse_buttons(&mut self) {
+        self.pressed_mouse_buttons = [false; MOUSE_BUTTON_COUNT];
+        self.released_mouse_buttons = [false; MOUSE_BUTTON_COUNT];
+    }
+
+    fn reset_gamepad_buttons(&mut self) {
+        self.pressed_gamepad_buttons = GamepadButtonState::ZERO;
+        self.released_gamepad_buttons = GamepadButtonState::ZERO;
+    }
+
+    fn reset_mouse_motion(&mut self) {
+        self.current_mouse_delta = Vector2f::new(0.0, 0.0);
         self.current_mouse_scroll_delta = Vector2f::new(0.0, 0.0);
         self.current_mouse_scroll_pixel_delta = Vector2f::new(0.0, 0.0);
     }
 
     // Keyboard keys
-    pub(crate) fn set_key(&mut self, key: KeyboardKey, state: ElementState) {
+    pub fn set_key(&mut self, key: KeyboardKey, state: ElementState) {
+        let i = key as usize;
         match state {
             ElementState::Pressed => {
-                if self.keyboard_keys[key as usize] {
-                    self.pressed_keyboard_keys[key as usize] = false;
+                if self.keyboard_keys[i] {
+                    self.pressed_keyboard_keys.set(i, false);
                 }
                 else {
-                    self.pressed_keyboard_keys[key as usize] = true;
-                    self.keyboard_keys[key as usize] = true;
+                    self.pressed_keyboard_keys.set(i, true);
+                    self.keyboard_keys.set(i, true);
                 }
             },
             ElementState::Released => {
-                self.released_keyboard_keys[key as usize] = true;
-                self.keyboard_keys[key as usize] = false;
+                self.released_keyboard_keys.set(i, true);
+                self.keyboard_keys.set(i, false);
             },
         }
     }
@@ -121,14 +167,14 @@ impl InputComponent {
     }
 
     // Mouse buttons
-    pub(crate) fn set_mouse_button(&mut self, button: MouseButton, state: ElementState) {
+    pub fn set_mouse_button(&mut self, button: MouseButton, state: ElementState) {
         let index = match button {
             MouseButton::Left => 0,
             MouseButton::Middle => 1,
             MouseButton::Right => 2,
             _ => return
         };
-        
+
         match state {
             ElementState::Pressed => {
                 if self.mouse_buttons[index] {
@@ -145,7 +191,7 @@ impl InputComponent {
             }
         }
     }
-    
+
     pub fn get_mouse_button_pressed(&self, button: MouseButton) -> bool {
         match button {
             MouseButton::Left => self.pressed_mouse_buttons[0],
@@ -178,7 +224,7 @@ impl InputComponent {
         self.current_mouse_scroll_delta
     }
 
-    pub(crate) fn set_mouse_scroll_delta(&mut self, delta: Vector2f) {
+    pub fn set_mouse_scroll_delta(&mut self, delta: Vector2f) {
         self.current_mouse_scroll_delta = delta;
     }
 
@@ -186,17 +232,17 @@ impl InputComponent {
         self.current_mouse_scroll_pixel_delta
     }
 
-    pub(crate) fn set_mouse_scroll_pixel_delta(&mut self, delta: Vector2f) {
+    pub fn set_mouse_scroll_pixel_delta(&mut self, delta: Vector2f) {
         self.current_mouse_scroll_pixel_delta = delta;
     }
 
     // - Mouse motion
-      
+
     pub fn get_mouse_delta(&self) -> Vector2f {
         self.current_mouse_delta
     }
 
-    pub(crate) fn set_mouse_delta(&mut self, delta: Vector2f) {
+    pub fn set_mouse_delta(&mut self, delta: Vector2f) {
         self.current_mouse_delta = delta;
     }
 
@@ -204,13 +250,54 @@ impl InputComponent {
         self.current_mouse_position
     }
 
-    pub(crate) fn set_mouse_position(&mut self, position: Vector2f) {
+    pub fn set_mouse_position(&mut self, position: Vector2f) {
         self.current_mouse_position = position;
+    }
+
+    // Gamepad buttons
+    pub fn set_gamepad_button(&mut self, button: GamepadButton, state: ElementState) {
+        let i = button as usize;
+        match state {
+            ElementState::Pressed => {
+                if self.gamepad_buttons[i] {
+                    self.pressed_gamepad_buttons.set(i, false);
+                }
+                else {
+                    self.pressed_gamepad_buttons.set(i, true);
+                    self.gamepad_buttons.set(i, true);
+                }
+            },
+            ElementState::Released => {
+                self.released_gamepad_buttons.set(i, true);
+                self.gamepad_buttons.set(i, false);
+            },
+        }
+    }
+
+    pub fn get_gamepad_button(&self, button: GamepadButton) -> bool {
+        self.gamepad_buttons[button as usize]
+    }
+
+    pub fn get_gamepad_button_pressed(&self, button: GamepadButton) -> bool {
+        self.pressed_gamepad_buttons[button as usize]
+    }
+
+    pub fn get_gamepad_button_released(&self, button: GamepadButton) -> bool {
+        self.released_gamepad_buttons[button as usize]
+    }
+
+    pub fn set_gamepad_axis(&mut self, axis: GamepadAxis, raw: f32) {
+        let v = if raw.abs() < GAMEPAD_DEADZONE { 0.0 } else { raw };
+        self.gamepad_axes[axis as usize] = raw;
+    }
+
+    pub fn get_gamepad_axis(&self, axis: GamepadAxis) -> f32 {
+        self.gamepad_axes[axis as usize]
     }
 }
 
 impl PillTypeMapKey for InputComponent {
-    type Storage = GlobalComponentStorage<InputComponent>; 
+    type Storage = GlobalComponentStorage<InputComponent>;
 }
 
-impl GlobalComponent for InputComponent { } 
+impl GlobalComponent for InputComponent { }
--- engine/pill_engine/src/ecs/systems/input_system.rs
+++ engine/pill_engine/src/ecs/systems/input_system.rs
@@ -1,6 +1,6 @@
-use crate::{ 
+use crate::{
     engine::Engine,
-    ecs::{ InputComponent, InputEvent },
+    ecs::{ InputComponent, InputEvent, GamepadAxis, GamepadButton },
 };
 
 use pill_core::{ Vector2f };
@@ -8,20 +8,39 @@ use pill_core::{ Vector2f };
 use anyhow::{ Result, Context, Error };
 use winit::event::{ ElementState, MouseButton, MouseScrollDelta };
 
-pub fn input_system(engine: &mut Engine) -> Result<()> {
-    let input_component = engine.get_global_component_mut::<InputComponent>()?;
-    input_component.set_keys();
-    input_component.set_mouse_buttons();
-    input_component.set_mouse_motion();
+// use a lazy static GILRS instance
+use gilrs::{ Gilrs, EventType, Axis, Button };
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
-    while engine.input_queue.is_empty() == false {
-        let front_event = engine.input_queue.pop_front().unwrap();
+static GILRS: Lazy<Mutex<Gilrs>> = Lazy::new(|| Mutex::new(Gilrs::new().expect("Failed to initialize Gilrs")));
+
+pub fn input_system(engine: &mut Engine) -> Result<()> {
+    // Poll GILRS first
+    {
+        let mut gilrs = GILRS.lock().unwrap();
+        while let Some(ev) = gilrs.next_event() {
+            match ev.event {
+                EventType::ButtonPressed(b, _) => engine.input_queue.push_back(InputEvent::GamepadButton { button: b.into(), state: ElementState::Pressed }),
+                EventType::ButtonReleased(b, _) => engine.input_queue.push_back(InputEvent::GamepadButton { button: b.into(), state: ElementState::Released }),
+                EventType::AxisChanged(a, v, _) => engine.input_queue.push_back(InputEvent::GamepadAxis { axis: a.into(), value: v }),
+                _ => {},
+            }
+        }
+    }
+
+    {
         let input_component = engine.get_global_component_mut::<InputComponent>()?;
-    
-        match front_event {
+        input_component.clear_transient_states();
+    }
+
+    while let Some(event) = engine.input_queue.pop_front() {
+        let input_component = engine.get_global_component_mut::<InputComponent>()?;
+
+        match event {
             // Keyboard keys
             InputEvent::KeyboardKey { key, state } => {
-                input_component.set_key(key, state); 
+                input_component.set_key(key, state);
             },
 
             // Mouse buttons
@@ -51,8 +70,63 @@ pub fn input_system(engine: &mut Engine) -> Result<()> {
             InputEvent::MousePosition { position} => {
                 input_component.set_mouse_position(position);
             },
+
+            // Gamepad buttons
+            InputEvent::GamepadButton { button, state } => {
+                input_component.set_gamepad_button(button, state);
+            },
+
+            // Gamepad axes
+            InputEvent::GamepadAxis { axis, value } => {
+                input_component.set_gamepad_axis(axis, value);
+            },
         }
     }
-    
+
     Ok(())
+}
+
+// GILRS -> input enum conversions
+impl From<Button> for GamepadButton {
+    fn from(button: Button) -> Self {
+        match button {
+            // ABXY
+            Button::South => GamepadButton::A,
+            Button::East => GamepadButton::B,
+            Button::North => GamepadButton::X,
+            Button::West => GamepadButton::Y,
+            // Triggers and bumpers
+            Button::LeftTrigger => GamepadButton::LeftBumper,
+            Button::LeftTrigger2 => GamepadButton::LeftTrigger,
+            Button::RightTrigger => GamepadButton::RightBumper,
+            Button::RightTrigger2 => GamepadButton::RightTrigger,
+            // Menus
+            Button::Select => GamepadButton::Back,
+            Button::Start => GamepadButton::Start,
+            Button::Mode => GamepadButton::Mode,
+            // DPad
+            Button::DPadUp => GamepadButton::DPadUp,
+            Button::DPadDown => GamepadButton::DPadDown,
+            Button::DPadLeft => GamepadButton::DPadLeft,
+            Button::DPadRight => GamepadButton::DPadRight,
+            // Sticks
+            Button::LeftThumb => GamepadButton::LeftStick,
+            Button::RightThumb => GamepadButton::RightStick,
+            _ => GamepadButton::Mode, // Handle other buttons as Mode
+        }
+    }
+}
+
+impl From<Axis> for GamepadAxis {
+    fn from(axis: Axis) -> Self {
+        match axis {
+            Axis::LeftStickX => GamepadAxis::LeftStickX,
+            Axis::LeftStickY => GamepadAxis::LeftStickY,
+            Axis::RightStickX => GamepadAxis::RightStickX,
+            Axis::RightStickY => GamepadAxis::RightStickY,
+            Axis::LeftZ => GamepadAxis::LeftTrigger,
+            Axis::RightZ => GamepadAxis::RightTrigger,
+            _ => GamepadAxis::LeftStickX, // Handle other axes as LeftStickX
+        }
+    }
 }
